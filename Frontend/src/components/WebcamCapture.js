568412cb0b052ebdc53e5440b2be3b7aec110b61
nadia3190 <nadia.tlb.90@gmail.com>
2024-07-26 22:53:08 +0200

    final commit

--- Frontend/src/components/WebcamCapture.js
+++ Frontend/src/components/WebcamCapture.js
@@ -1,8 +1,10 @@
 import React, { useRef, useEffect, useCallback, useState } from "react";
 import Webcam from "react-webcam";
 import * as faceapi from "face-api.js";
+import "./WebcamCapture.css";
 
 const MODEL_URL = "/facialjs/models";
+
 const WebcamCapture = () => {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
@@ -11,20 +13,11 @@ const WebcamCapture = () => {
   const loadModels = async () => {
     try {
       await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
-      console.log("Tiny Face Detector model loaded");
-
       await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
-      console.log("Face Landmark 68 model loaded");
-
       await faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL);
-      console.log("Face Recognition model loaded");
-
       await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
-      console.log("Face Expression model loaded");
-
       await faceapi.nets.ageGenderNet.loadFromUri(MODEL_URL);
-      console.log("Age Gender model loaded");
-
+      console.log("All models loaded");
       setModelsLoaded(true);
     } catch (error) {
       console.error("Error loading models", error);
@@ -38,46 +31,57 @@ const WebcamCapture = () => {
       modelsLoaded
     ) {
       const video = webcamRef.current.video;
-      const detections = await faceapi
-        .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-        .withFaceLandmarks()
-        .withFaceExpressions()
-        .withAgeAndGender();
 
-      const canvas = canvasRef.current;
-      const displaySize = {
-        width: video.videoWidth,
-        height: video.videoHeight,
-      };
-      faceapi.matchDimensions(canvas, displaySize);
-      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      console.log("Video Dimensions: ", video.videoWidth, video.videoHeight);
 
-      const ctx = canvas.getContext("2d");
-      ctx.clearRect(0, 0, canvas.width, canvas.height); // Effacer le canevas avant de dessiner
+      if (video.videoWidth > 0 && video.videoHeight > 0) {
+        const detections = await faceapi
+          .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+          .withFaceLandmarks()
+          .withFaceExpressions()
+          .withAgeAndGender();
 
-      resizedDetections.forEach((detection) => {
-        const { x, y, width, height } = detection.detection.box;
-        ctx.strokeStyle = "red";
-        ctx.lineWidth = 2;
-        ctx.strokeRect(x, y, width, height);
-
-        const { age, gender, expressions } = detection;
-        const drawText = (text, x, y) => {
-          ctx.font = "16px Arial";
-          ctx.fillStyle = "red";
-          ctx.fillText(text, x, y);
+        const canvas = canvasRef.current;
+        const displaySize = {
+          width: video.videoWidth,
+          height: video.videoHeight,
         };
 
-        drawText(`Age: ${Math.round(age)}`, x, y - 20);
-        drawText(`Gender: ${gender}`, x, y - 40);
-        drawText(
-          `Expressions: ${Object.keys(expressions)
-            .map((e) => `${e}: ${Math.round(expressions[e] * 100) / 100}`)
-            .join(", ")}`,
-          x,
-          y - 60
+        faceapi.matchDimensions(canvas, displaySize);
+        const resizedDetections = faceapi.resizeResults(
+          detections,
+          displaySize
         );
-      });
+
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Effacer le canevas avant de dessiner
+
+        resizedDetections.forEach((detection) => {
+          const { x, y, width, height } = detection.detection.box;
+          ctx.strokeStyle = "red";
+          ctx.lineWidth = 2;
+          ctx.strokeRect(x, y, width, height);
+
+          const { age, gender, expressions } = detection;
+          const drawText = (text, x, y) => {
+            ctx.font = "16px Arial";
+            ctx.fillStyle = "red";
+            ctx.fillText(text, x, y);
+          };
+
+          drawText(`Age: ${Math.round(age)}`, x, y - 20);
+          drawText(`Gender: ${gender}`, x, y - 40);
+          drawText(
+            `Expressions: ${Object.keys(expressions)
+              .map((e) => `${e}: ${Math.round(expressions[e] * 100) / 100}`)
+              .join(", ")}`,
+            x,
+            y - 60
+          );
+        });
+      } else {
+        console.error("Invalid video dimensions");
+      }
     }
   }, [webcamRef, modelsLoaded]);
 
@@ -95,24 +99,17 @@ const WebcamCapture = () => {
   }, [detect, modelsLoaded]);
 
   return (
-    <div style={{ position: "relative", width: "640px", height: "480px" }}>
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        width={640}
-        height={480}
-      />
-      <canvas
-        ref={canvasRef}
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "640px",
-          height: "480px",
-        }}
-      />
+    <div className="container">
+      <div className="webcam-wrapper">
+        <Webcam
+          audio={false}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          width="100%"
+          height="100%"
+        />
+        <canvas ref={canvasRef} />
+      </div>
     </div>
   );
 };
